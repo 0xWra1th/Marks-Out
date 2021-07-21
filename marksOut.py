7611cc20d862c59792c7f88096cb7a1f268d3d2d
0xWra1th <danielscragg@outlook.com>
2021-07-21 12:11:25 +0200

    Fixed Loop, tested and it works.

--- marksOut.py
+++ marksOut.py
@@ -9,10 +9,11 @@ import sys
 import subprocess
 
 # VARIABLES
-USERNAME = str(sys.argv[1])
-PASSWORD = str(sys.argv[2])
-LOGIN_EMAIL = str(sys.argv[1])+"@tuks.co.za"
-ALERT_EMAILS = [LOGIN_EMAIL]
+USERNAME = ""
+PASSWORD = ""
+LOGIN_EMAIL = ""
+ALERT_EMAILS = [LOGIN_EMAIL, '']
+SENDER = ""
 prevMarks = []
 marks = []
 
@@ -22,6 +23,7 @@ def poll():
 	op = webdriver.ChromeOptions()
 	op.add_argument('--headless')
 	driver = webdriver.Chrome(options=op)
+	#driver = webdriver.Chrome()
 	driver.get("https://upnet.up.ac.za")
 
 	time.sleep(1)
@@ -35,11 +37,13 @@ def poll():
 	passField.send_keys(PASSWORD)
 	passField.send_keys(Keys.RETURN)
 
+	time.sleep(3)
+
 	if "PROCEED WITHOUT PASSWORD CHANGE" in driver.page_source:
 		try:
 			driver.find_element_by_partial_link_text('PROCEED').click()
 		except:
-			time.sleep(3)
+			time.sleep(5)
 			driver.find_element_by_partial_link_text('PROCEED').click()
 
 	oldTab = driver.current_window_handle
@@ -69,7 +73,7 @@ def poll():
 			time.sleep(10)
 			driver.find_element_by_id("win0divPTNUI_LAND_REC_GROUPLET$2").click()
 
-	time.sleep(3)
+	time.sleep(5)
 
 	marks = []
 
@@ -103,9 +107,11 @@ while(True):
 	try:
 		marks = poll()
 		for mark in marks:
-			if mark not in prevMarks and prevMarks != []:
+			print(mark)
+			if mark not in prevMarks and mark[1] != '&nbsp':
 				alert(mark)
 		prevMarks = marks
 		time.sleep(60)
-	except:
+	except Exception as e:
+		print(e)
 		pass
